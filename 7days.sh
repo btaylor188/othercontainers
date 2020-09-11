50cc13eec7c5b9d0748d5de780dd061511f10294
btaylor188 <64984258+btaylor188@users.noreply.github.com>
2020-09-10 23:39:51 -0600

    Update 7days.sh

--- 7days.sh
+++ 7days.sh
@@ -1,7 +1,9 @@
 docker run \
   --name 7dtd \
   --restart unless-stopped \
-  -v "/mnt/nvme/docker/7dtd:/home/sdtdserver" \
+  -v "/mnt/nvme/docker/7dtd/7DaysToDie:/home/sdtdserver/.local/share/7DaysToDie/" \
+  -v "/mnt/nvme/docker/7dtd/ServerFiles:/home/sdtdserver/serverfiles/" \
+  -v "/mnt/nvme/docker/7dtd/LogFolder:/home/sdtdserver/log/" \
   -p 26900:26900/tcp \
   -p 26900:26900/udp \
   -p 26901:26901/udp \
@@ -10,7 +12,6 @@ docker run \
   -p 9023:9023/tcp \
   -e START_MODE=1 \
   -e VERSION=stable \
-  -e TEST_ALERT=YES \
   -e PUID=1000 \
   -e PGID=1000 \
   -e TimeZone=America/Denver \
