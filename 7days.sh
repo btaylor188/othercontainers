aa6200ce6217f2fe7c56ab570972d7266e8c03a5
btaylor188 <64984258+btaylor188@users.noreply.github.com>
2020-09-10 22:20:54 -0600

    Create 7days.sh

---
+++ 7days.sh
@@ -0,0 +1,22 @@
+docker run \
+  --name 7dtdserver \
+  --restart unless-stopped \
+  -v "./7DaysToDie:/home/sdtdserver/.local/share/7DaysToDie/" \
+  -v "./ServerFiles:/home/sdtdserver/serverfiles/" \
+  -v "./LogFolder:/home/sdtdserver/log/" \
+  -v "./BackupFolder:/home/sdtdserver/lgsm/backup/" \
+  -v "./LGSM-Config:/home/sdtdserver/lgsm/config-lgsm/sdtdserver/" \
+  -p 26900:26900/tcp \
+  -p 26900:26900/udp \
+  -p 26901:26901/udp \
+  -p 26902:26902/udp \
+  -p 8080:8080/udp \
+  -p 8081:8081/tcp \
+  -p 8082:8082/tcp \
+  -e START_MODE=1 \
+  -e VERSION=stable \
+  -e TEST_ALERT=YES \
+  -e PUID=1000 \
+  -e PGID=1000 \
+  -e TimeZone=Europe/Madrid \
+  vinanrra/7dtd-server
