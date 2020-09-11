6a8b451412acfba9413ab2b2846c52341bd65fe7
btaylor188 <64984258+btaylor188@users.noreply.github.com>
2020-09-10 23:34:42 -0600

    Update 7days.sh

--- 7days.sh
+++ 7days.sh
@@ -1,6 +1,17 @@
-docker run -d -v /mnt/nvme/docker/7dtd/data:/data \
+docker run \
   --name 7dtd \
-  --restart=always \
-  -p 26900-26902:26900-26902 \
-  -p 26900-26902:26900-26902/udp \
-  zobees/7daystodie
+  --restart unless-stopped \
+  -v "/mnt/nvme/docker/7dtd:/home/sdtdserver" \
+  -p 26900:26900/tcp \
+  -p 26900:26900/udp \
+  -p 26901:26901/udp \
+  -p 26902:26902/udp \
+  -p 9080:9080/udp \
+  -p 9023:9023/tcp \
+  -e START_MODE=1 \
+  -e VERSION=stable \
+  -e TEST_ALERT=YES \
+  -e PUID=1000 \
+  -e PGID=1000 \
+  -e TimeZone=America/Denver \
+  vinanrra/7dtd-server
