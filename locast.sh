79c3066f6188cf8d9602afdcfae546f13c9305fa
btaylor188 <64984258+btaylor188@users.noreply.github.com>
2020-09-13 15:33:22 -0600

    Update locast.sh

--- locast.sh
+++ locast.sh
@@ -3,5 +3,5 @@ docker run -d \
 --restart=always \
 -p 6077:6077
 -p 1900:1900/udp \
--v /mnt/nvme/docker/locast/config.ini:/app/config/config.ini
+-v /mnt/nvme/docker/locast/config.ini:/app/config/config.ini \
 tgorg/locast2plex
