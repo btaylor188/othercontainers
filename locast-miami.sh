f72acc0d817db8cb6b71fbb57e157f8d24eeb1ad
btaylor188 <64984258+btaylor188@users.noreply.github.com>
2020-09-14 16:08:41 -0600

    added locast to srcipt

--- locast-miami.sh
+++ locast-miami.sh
@@ -3,18 +3,25 @@ echo "Username?"
 read USERNAME
 echo "Password?"
 read PASSWORD
-echo "PIA Region?"
-read REGION
 
-docker network create pia_network
+
 
 docker run -d \
   --cap-add=NET_ADMIN \
   --name=pia \
-  --network=pia_network \
+  --network=internal \
   --restart=always \
   --dns 209.222.18.222 --dns 209.222.18.218 \
-  -e REGION=$REGION \
+  -p 6078:6077 \
+  -e REGION="US Florida" \
   -e USERNAME=$USERNAME \
   -e PASSWORD=$PASSWORD \
   itsdaspecialk/pia-openvpn
+
+
+docker run -d \
+  --name=locast-miami \
+  --net=container:pia \
+  -v /mnt/nvme/docker/locast-miami:/app/config \
+  tgorg/locast2plex
+
