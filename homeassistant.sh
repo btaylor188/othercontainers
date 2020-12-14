882e5c08f23c4b63aaf6e9ccc3f6fa79be65aa56
btaylor188 <64984258+btaylor188@users.noreply.github.com>
2020-12-14 15:52:58 -0700

    Update homeassistant.sh

--- homeassistant.sh
+++ homeassistant.sh
@@ -9,9 +9,21 @@ docker run -d \
 --name=homeassistant \
 --restart=always \
 --net=host \
-
 -v /$DOCKERPATH/homeassistant:/config \
 -v /etc/localtime:/etc/localtime:ro \
+-l "traefik.enable"="true" \
+-l "traefik.frontend.auth.forward.address"="http://oauth:4181" \
+-l "traefik.frontend.headers.SSLHost"="$DOMAINNAME" \
+-l "traefik.frontend.headers.SSLRedirect"="true" \
+-l "traefik.frontend.headers.STSIncludeSubdomains"="true" \
+-l "traefik.frontend.headers.STSPreload"="true" \
+-l "traefik.frontend.headers.STSSeconds"="315360000" \
+-l "traefik.frontend.headers.browserXSSFilter"="true" \
+-l "traefik.frontend.headers.contentTypeNosniff"="true" \
+-l "traefik.frontend.headers.customResponseHeaders"="X-Robots-Tag:noindex,nofollow,nosnippet,noarchive,notranslate,noimageindex" \
+-l "traefik.frontend.headers.forceSTSHeader"="true" \
+-l "traefik.frontend.rule"="Host:homeassistant.$DOMAINNAME" \
+-l "traefik.port"="8123" \
 homeassistant/home-assistant:stable
 
 
