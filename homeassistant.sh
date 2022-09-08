ef4ffab5b9a5abaa7e2d7a26b5cc32723fe702cc
btaylor188 <64984258+btaylor188@users.noreply.github.com>
2022-09-08 12:55:27 -0600

    a

--- homeassistant.sh
+++ homeassistant.sh
@@ -5,25 +5,16 @@ read DOCKERPATH
 echo "What is the domain name?"
 read DOMAINNAME
 
-docker run -d \
---name=homeassistant \
---restart=always \
---net=host \
--v /$DOCKERPATH/homeassistant:/config \
--l "traefik.enable"="true" \
--l "traefik.frontend.auth.forward.address"="http://oauth:4181" \
--l "traefik.frontend.headers.SSLHost"="$DOMAINNAME" \
--l "traefik.frontend.headers.SSLRedirect"="true" \
--l "traefik.frontend.headers.STSIncludeSubdomains"="true" \
--l "traefik.frontend.headers.STSPreload"="true" \
--l "traefik.frontend.headers.STSSeconds"="315360000" \
--l "traefik.frontend.headers.browserXSSFilter"="true" \
--l "traefik.frontend.headers.contentTypeNosniff"="true" \
--l "traefik.frontend.headers.customResponseHeaders"="X-Robots-Tag:noindex,nofollow,nosnippet,noarchive,notranslate,noimageindex" \
--l "traefik.frontend.headers.forceSTSHeader"="true" \
--l "traefik.frontend.rule"="Host:homeassistant.$DOMAINNAME" \
--l "traefik.port"="8123" \
-homeassistant/home-assistant:stable
+echo "Assign Unused Network IP"
+read $IP
 
+cat > ./.env << EOF1
+DOMAINNAME=$DOMAINNAME
+DOCKERPATH=$DOCKERPATH
+IP=$IP
+EOF1
+
+sudo docker-compose -f homeassistant.yaml -p homeassistant up -d
+rm ./.env
 
 
