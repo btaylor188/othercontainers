3272373ce8b0039e7b2cd257b3904895924872b8
btaylor188 <64984258+btaylor188@users.noreply.github.com>
2022-09-08 14:23:08 -0600

    a

--- homeassistant.sh
+++ homeassistant.sh
@@ -2,14 +2,10 @@
 echo "Enter path for Docker data.  ie. /mnt/docker"
 read DOCKERPATH
 
-echo "What is the domain name?"
-read DOMAINNAME
-
 echo "Assign Unused Network IP"
 read IP
 
 cat > ./.env << EOF1
-DOMAINNAME=$DOMAINNAME
 DOCKERPATH=$DOCKERPATH
 IP=$IP
 EOF1
--- unifi.sh
+++ unifi.sh
@@ -1,12 +1,13 @@
 #! /bin/bash
-echo "What is the domain name?"
-read DOMAINNAME
-
 echo "Enter path for Docker data.  ie. /mnt/docker"
 read DOCKERPATH
+
+echo "Assign Unused Network IP"
+read IP
+
 cat > ./.env << EOF1
-DOMAINNAME=$DOMAINNAME
 DOCKERPATH=$DOCKERPATH
+IP=$IP
 EOF1
 
 sudo docker-compose -f unifi.yaml -p unifi up -d
